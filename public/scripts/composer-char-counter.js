5e24022e00b17d8e3a0221c2b751c87323ecba0b
Alex M <milesAwayAlex@gmail.com>
2021-07-13 18:53:13 -0700

    Add the tweet article section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,10 @@
 const charLimit = 140;
+
 $(document).ready(() => {
+  const counter = $('#counter');
+  counter.text(charLimit);
   $('#tweet-text').on('input', (e) => {
     const count = charLimit - e.target.value.length;
-    const counter = $('#counter');
     counter.text(count);
     if (count < 0) counter.addClass('red');
     if (count >= 0) counter.removeClass('red');
